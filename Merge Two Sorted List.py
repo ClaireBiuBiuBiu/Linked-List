348674eac481e2358d6f75f418b9878e25b16e93
Cloris <hlyy1994>
2020-10-24 12:22:25 -0400

    Merge two sorted list

---
+++ Merge Two Sorted List.py
@@ -0,0 +1,24 @@
+''''
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+注意原来就是sorted 所以用index可以直接找到最大的
+ '''
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        # maintain an unchanging reference to node ahead of the return node.
+        prehead = ListNode(-1)
+
+        prev = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        # exactly one of l1 and l2 can be non-null at this point, so connect
+        # the non-null list to the end of the merged list.
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
