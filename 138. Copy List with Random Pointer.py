e0caaf9b5fb9d92f64f266512d4562503e38758a
Cloris <hlyy1994>
2020-10-25 17:24:26 -0400

    Copy List with Random Pointer
    Solution

---
+++ 138. Copy List with Random Pointer.py
@@ -0,0 +1,50 @@
+''''
+A linked list is given such that each node contains an additional random pointer which could point to
+any node in the list or null.
+
+Return a deep copy of the list.
+
+The Linked List is represented in the input/output as a list of n nodes.
+Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to,
+or null if it does not point to any node.'''
+
+# Method 1 recursive
+class Solution(object):
+    """
+    :type head: Node
+    :rtype: Node
+    """
+    def __init__(self):
+        # Dictionary which holds old nodes as keys and new nodes as its values.
+        self.visitedHash = {}
+
+    def copyRandomList(self, head):
+
+        if head == None:
+            return None
+
+        # If we have already processed the current node, then we simply return the cloned version of it.
+        if head in self.visitedHash:
+            return self.visitedHash[head]
+
+        # create a new node
+        # with the value same as old node.
+        node = Node(head.val, None, None)
+
+        # Save this value in the hash map. This is needed since there might be
+        # loops during traversal due to randomness of random pointers and this would help us avoid them.
+        self.visitedHash[head] = node
+
+        # Recursively copy the remaining linked list starting once from the next pointer and then from the random pointer.
+        # Thus we have two independent recursive calls.
+        # Finally we update the next and random pointers for the new node created.
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+
+        return node
+
+
+#Method 2 iterative
